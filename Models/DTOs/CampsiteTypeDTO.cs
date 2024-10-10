0f969eb45b5f25543b5a15e5f8d30e32ed7cdd2d
Saygin Gecener <sgecener@yahoo.com>
2024-10-10 11:58:52 -0400

    caampsite type dto

---
+++ Models/DTOs/CampsiteTypeDTO.cs
@@ -0,0 +1,8 @@
+namespace CreekRiver.Models.DTOs;
+
+public class CampsiteTypeDTO {
+    public int Id { get; set; }
+    public string CampsiteTypeName { get; set; }
+    public int MaxReservationDays { get; set; }
+    public decimal FeePerNight { get; set; }
+}
