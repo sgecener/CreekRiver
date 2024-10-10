1b99e36c3abb0b73f5a95cb8d11b963a150d5da3
Saygin Gecener <sgecener@yahoo.com>
2024-10-10 11:22:12 -0400

    campsite dto

---
+++ Models/DTOs/CampsiteDTO.cs
@@ -0,0 +1,8 @@
+namespace CreekRiver.Models.DTOs;
+
+public class CampsiteDTO {
+    public int Id { get; set; }
+    public string Nickname { get; set; }
+    public string ImageUrl { get; set; }
+    public int CampsiteTypeId { get; set; }
+}
