5725e62274c47bf6ed30c2e8bcfebe2b9d5d4aed
Saygin Gecener <sgecener@yahoo.com>
2024-10-17 15:30:46 -0400

    db context add

---
+++ CreekRiverDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using CreekRiver.Models;
+
+public class CreekRiverDbContext : DbContext
+{
+
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<Campsite> Campsites { get; set; }
+    public DbSet<CampsiteType> CampsiteTypes { get; set; }
+
+    public CreekRiverDbContext(DbContextOptions<CreekRiverDbContext> context) : base(context)
+    {
+
+    }
+}
