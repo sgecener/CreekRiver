c0e664639300944a0fccdbcf04e64426b266799f
Saygin Gecener <sgecener@yahoo.com>
2024-10-17 15:36:21 -0400

    on model creating class added

--- CreekRiverDbContext.cs
+++ CreekRiverDbContext.cs
@@ -11,6 +11,17 @@ public class CreekRiverDbContext : DbContext
 
     public CreekRiverDbContext(DbContextOptions<CreekRiverDbContext> context) : base(context)
     {
-
+        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // seed data with campsite types
+        modelBuilder.Entity<CampsiteType>().HasData(new CampsiteType[]
+        {
+            new CampsiteType {Id = 1, CampsiteTypeName = "Tent", FeePerNight = 15.99M, MaxReservationDays = 7},
+            new CampsiteType {Id = 2, CampsiteTypeName = "RV", FeePerNight = 26.50M, MaxReservationDays = 14},
+            new CampsiteType {Id = 3, CampsiteTypeName = "Primitive", FeePerNight = 10.00M, MaxReservationDays = 3},
+            new CampsiteType {Id = 4, CampsiteTypeName = "Hammock", FeePerNight = 12M, MaxReservationDays = 7}
+        });
     }
 }
