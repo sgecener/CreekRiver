4b69ecb6a9d75b3e20d5e8aee639a37652796cbf
Saygin Gecener <sgecener@yahoo.com>
2024-11-08 11:04:01 -0500

    GET method reservations

--- Models/DTOs/ReservationDTO.cs
+++ Models/DTOs/ReservationDTO.cs
@@ -3,9 +3,9 @@ namespace CreekRiver.Models.DTOs;
 public class ReservationDTO {
     public int Id { get; set; }
     public int CampsiteId { get; set; }
-    public Campsite Campsite { get; set; }
+    public CampsiteDTO Campsite { get; set; }
     public int UserProfileId { get; set; }
-    public UserProfile UserProfile { get; set; }
-    public DateTime CheckInDate { get; set; }
+    public UserProfileDTO UserProfile { get; set; }
+    public DateTime CheckinDate { get; set; }
     public DateTime CheckoutDate { get; set; }
 }
